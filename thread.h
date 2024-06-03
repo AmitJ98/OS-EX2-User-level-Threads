fd3ce7bf666978f5b906f32f3b8d2e42e8208d03
AmitJ98 <143286440+AmitJ98@users.noreply.github.com>
2024-06-03 13:06:22 +0300

    Add files via upload
    initial

---
+++ thread.h
@@ -0,0 +1,39 @@
+#ifndef _THREAD_H_
+#define _THREAD_H_
+
+#include <setjmp.h>
+#include <sys/time.h>
+
+typedef void (*thread_entry_point)(void);
+typedef unsigned long long address_t; //?
+
+
+
+class thread
+{
+  private:
+    int _id;
+    int _state; // 0 - ready, 1 - running , 2 - blocked
+    int _quantum_counter;
+    address_t _sp;
+    address_t _pc;
+    char *_stack;
+    void (*_f)();
+    sigjmp_buf _env;
+
+
+
+ public:
+    thread(int id , void (*f)(void) );
+    ~thread();
+
+
+    int get_id() const;
+    int get_state()const;
+    int get_quantum_counter() const;
+
+
+    void set_state(int change_to);
+};
+
+#endif //_THREAD_H_
