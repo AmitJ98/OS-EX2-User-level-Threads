b801b1b841c4dd9b24489122095f890935aa47ba
mayaheilbrun1 <maya.heilbrun@mail.huji.ac.il>
2024-06-05 13:21:23 +0300

    added error masseges + readmefile

---
+++ cmake-build-debug/CMakeFiles/Os_Ex2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Os_Ex2.dir/demo_itimer.c.o"
+  "CMakeFiles/Os_Ex2.dir/demo_itimer.c.o.d"
+  "CMakeFiles/Os_Ex2.dir/demo_jmp.c.o"
+  "CMakeFiles/Os_Ex2.dir/demo_jmp.c.o.d"
+  "CMakeFiles/Os_Ex2.dir/demo_singInt_handler.c.o"
+  "CMakeFiles/Os_Ex2.dir/demo_singInt_handler.c.o.d"
+  "CMakeFiles/Os_Ex2.dir/thread.cpp.o"
+  "CMakeFiles/Os_Ex2.dir/thread.cpp.o.d"
+  "CMakeFiles/Os_Ex2.dir/uthreads.cpp.o"
+  "CMakeFiles/Os_Ex2.dir/uthreads.cpp.o.d"
+  "Os_Ex2"
+  "Os_Ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Os_Ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Os_Ex2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Os_Ex2.
+# This may be replaced when dependencies are built.
